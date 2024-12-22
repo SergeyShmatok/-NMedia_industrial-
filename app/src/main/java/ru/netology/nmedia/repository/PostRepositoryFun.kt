ca1b45ffd8714d7b9068fb845fafebe68d6f97fb
Sergey Shmatok <sergey.psy@mail.ru>
2024-12-22 03:01:04 +0300

    Added: asynchronous calls implemented

--- app/src/main/java/ru/netology/nmedia/repository/PostRepositoryFun.kt
+++ app/src/main/java/ru/netology/nmedia/repository/PostRepositoryFun.kt
@@ -3,9 +3,18 @@ package ru.netology.nmedia.repository
 import ru.netology.nmedia.dto.Post
 
 interface PostRepositoryFun {
-    fun getAll(): List<Post>
-    fun likeById(id: Long): Post
-    fun save(post: Post)
-    fun removeById(id: Long)
-    fun removeLike(id: Long): Post
+    //fun getAll(): List<Post>
+    fun getAllAsync(callback: NMediaCallback<List<Post>>)
+    fun likeById(id: Long, callback: NMediaCallback<Post>)
+    fun save(post: Post, callback: NMediaCallback<Post>)
+    fun removeById(id: Long, callback: NMediaCallback<Post>)
+    fun removeLike(id: Long, callback: NMediaCallback<Post>)
+
+interface NMediaCallback <T> {
+    fun onSuccess(data: T)
+    fun onError(e: Exception)
 }
+
+}
+
+
